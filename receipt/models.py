42b8c0a4caabbca51fbddb9bd0e4a1da34c550f0
Roberto Nuñez <rcorralesn@gmail.com>
2026-08-03 12:24:04 -0600

    fix: change image_url field from URLField to CharField for flexibility

--- receipt/models.py
+++ receipt/models.py
@@ -69,7 +69,7 @@ class Receipt(models.Model):
     subtotal_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     discount_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     store_name = models.CharField(max_length=255, null=True, blank=True)
-    image_url = models.URLField()
+    image_url = models.CharField(max_length=255, null=True, blank=True)
     source_type = models.CharField(
         max_length=32,
         choices=ReceiptSource.choices,
